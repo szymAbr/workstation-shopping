dc54d7e4456ed908fc6518b227c21b39117d51a0
szymAbr <sz.abramowski@gmail.com>
2022-04-06 19:28:07 +0200

    add state and input change handler

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import { Container, Row, Col } from "react-bootstrap";
 import WorkstationTable from "./components/WorkstationTable";
@@ -5,6 +6,8 @@ import Header from "./components/Header";
 import UserInput from "./components/UserInput";
 
 function App() {
+  const [items, setItems] = useState([]);
+
   return (
     <div className="App">
       <Header />
@@ -12,11 +15,11 @@ function App() {
       <Container>
         <Row className="mt-4">
           <Col xs={12} md={5} lg={4}>
-            <UserInput />
+            <UserInput setItems={setItems} />
           </Col>
 
           <Col xs={12} md={7} lg={8}>
-            <WorkstationTable />
+            <WorkstationTable items={items} />
           </Col>
         </Row>
       </Container>
--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,13 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Form, Button } from "react-bootstrap";
 
-export default function UserInput() {
+export default function UserInput({ setItems }) {
+  const [currentItem, setCurrentItem] = useState({
+    name: "",
+    details: "",
+    category: "",
+    price: 0,
+  });
+
+  function handleChange(event) {
+    const propName = event.target.name;
+    const propValue = event.target.value;
+
+    setCurrentItem({
+      ...currentItem,
+      [propName]: propValue,
+    });
+  }
+
+  useEffect(() => {
+    console.log(currentItem);
+  }, [currentItem]);
+
   return (
     <>
       <h4 className="mt-4">Add items to build your perfect workstation.</h4>
 
       <Form>
-        <Form.Group className="my-3" controlId="formItemName">
+        <Form.Group
+          className="my-3"
+          name="name"
+          controlId="formItemName"
+          onChange={handleChange}
+        >
           <Form.Control type="text" placeholder="Name" />
 
           {/* <Form.Text className="text-muted">
@@ -15,23 +41,38 @@ export default function UserInput() {
         </Form.Text> */}
         </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formItemDetails">
+        <Form.Group
+          className="mb-3"
+          name="details"
+          controlId="formItemDetails"
+          onChange={handleChange}
+        >
           <Form.Control type="text" placeholder="Details" />
         </Form.Group>
 
-        <Form.Select className="mb-3" aria-label="Item category selection">
+        <Form.Select
+          className="mb-3"
+          name="category"
+          aria-label="Item category selection"
+          onChange={handleChange}
+        >
           <option>Category</option>
 
-          <option value="1">Computer components</option>
+          <option value="Computer components">Computer components</option>
 
-          <option value="2">Peripherals</option>
+          <option value="Peripherals">Peripherals</option>
 
-          <option value="3">Software</option>
+          <option value="Software">Software</option>
 
-          <option value="4">Other</option>
+          <option value="Other">Other</option>
         </Form.Select>
 
-        <Form.Group className="mb-3" controlId="formItemPrice">
+        <Form.Group
+          className="mb-3"
+          name="price"
+          controlId="formItemPrice"
+          onChange={handleChange}
+        >
           <Form.Control type="text" placeholder="Price" />
         </Form.Group>
 
